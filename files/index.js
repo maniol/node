f488f7187d3f57a43b73e44f941e805968121812
magda <magdalena.aniol@gmail.com>
2018-07-02 11:20:10 +0200

    add server exercise

--- files/index.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-fs.readdir('.', function(err, files) {
-	var s = files.join('\n');
-	fs.writeFile('./dirList.txt', s, function(err) {
-		if (err) throw err; // dlaczego nie ma {}
-		console.log('Zapisano!');
-	});
-});
