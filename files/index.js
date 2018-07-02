629cd5fd51de520232768c12922dfc13c55385c4
magda <magdalena.aniol@gmail.com>
2018-07-02 11:53:04 +0200

    correct var name

--- files/index.js
+++ files/index.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
 
 fs.readdir('.', function(err, files) {
-	var FilesListString = files.join('\n');
-	fs.writeFile('./dirList.txt', FilesListString, function(err) {
+	var filesListString = files.join('\n');
+	fs.writeFile('./dirList.txt', filesListString, function(err) {
 		if (err) throw err; // dlaczego nie ma {}
 		console.log('Zapisano!');
 	});
