7e7d1882d5ff5e34ada45653800d9963cbff7b57
magda <magdalena.aniol@gmail.com>
2018-07-03 10:12:10 +0200

    fix indentation

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ var server = http.createServer();
 server.on('request', function (request, response) {
 	response.setHeader('Content-Type', 'text/html; charset=utf-8');
 	if (request.method === 'GET' && request.url ==='/') {
-		fs.readFile('index.html', function(err, html){
+		fs.readFile('index.html', function(err, html) {
 			response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
 			response.write(html);
 			response.end();
 		});
 	} else {
-					fs.readFile('404.jpg', 'binary', function(error, file){
-						response.writeHead(404, {'Content-Type': 'image/jpg'});
-						response.write(file, 'binary');
-						response.end();
-					});
+			fs.readFile('404.jpg', 'binary', function(error, file) {
+				response.writeHead(404, {'Content-Type': 'image/jpg'});
+				response.write(file, 'binary');
+				response.end();
+			});
 	}
 });
 server.listen(8080);
